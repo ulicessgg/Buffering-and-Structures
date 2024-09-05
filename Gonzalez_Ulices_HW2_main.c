42519d6da4de8f06f038777035c2f755f1f160bb
Ulices Gonzalez <sgonzalez30@sfsu.edu>
2024-09-05 15:02:58 -0700

    edited makefile for compiliation and execution and setup main c file

---
+++ Gonzalez_Ulices_HW2_main.c
@@ -0,0 +1,23 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2024
+* Name:: Ulices Gonzalez
+* Student ID:: 923328897
+* GitHub-Name:: ulicessgg
+* Project:: Assignment 2 – Stucture in Memory and Buffering
+*
+* File:: Gonzalez_Ulices_HW2_main.c
+*
+* Description::
+*
+**************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "assignment2.h"
+
+int main(int argc, char* argv[])
+{
+    printf("Hello World!");
+
+    return 0;
+}
