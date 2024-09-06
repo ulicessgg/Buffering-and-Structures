56127b65cdd8d98e281a95573f112e405bfc70dc
Ulices Gonzalez <sgonzalez30@sfsu.edu>
2024-09-05 19:46:24 -0700

    implemented the use of malloc to allocate the pi instance and have run a successful execution

--- Gonzalez_Ulices_HW2_main.c
+++ Gonzalez_Ulices_HW2_main.c
@@ -17,7 +17,38 @@
 
 int main(int argc, char* argv[])
 {
-    printf("Hello World!");
+    // malloc allocates the memory for pi by returning a pointer of the
+    // size of the personalInfo struct or null if it is unable to allocate
+    personalInfo* pi = malloc(sizeof(struct personalInfo));
+
+    //testing pi to make sure it isnt null
+    if(pi == NULL)
+    {
+        printf("\nmalloc failed to allocate pi\n");
+        return 0;
+    }
+
+    // sets the values of the pi values using the first two command line arguements
+    // and with hardcoded values
+    pi->firstName = argv[1];
+    pi->lastName = argv[2];
+    pi->studentID = 923328897;
+    pi->level = SENIOR;
+    pi->languages = KNOWLEDGE_OF_C && KNOWLEDGE_OF_JAVA &&
+                    KNOWLEDGE_OF_JAVASCRIPT && KNOWLEDGE_OF_PYTHON &&
+                    KNOWLEDGE_OF_CPLUSPLUS && KNOWLEDGE_OF_SQL &&
+                    KNOWLEDGE_OF_HTML && KNOWLEDGE_OF_MIPS_ASSEMBLER &&
+                    KNOWLEDGE_OF_R && KNOWLEDGE_OF_BASIC;
+    strncpy(pi->message, argv[3], sizeof(pi->message));
+
+    // testing writePersonalInfo with pi to make sure it runs sucessfully
+    if(writePersonalInfo(pi) != 0)
+    {
+        printf("\nwrite personal failed to write pi and did not return 0\n");
+        return 0;
+    }
+
+    free(pi);
 
     return 0;
 }
